1d461e117bc5d63c47e05f6d2fc9eb7a84e639e1
Mariami Makhniashvili <maro.maxniashvili98@gmail.com>
2023-12-19 22:47:09 +0100

    )

--- app/componets/Button/Button.tsx
+++ app/componets/Button/Button.tsx
@@ -1,20 +1,20 @@
 import styles from '../Button/Button.module.scss'
 type Props = {
-   title : string;
-   className?: string;
-   disabled?: boolean;
-   mode?: 'fill' | 'outline';
+    title: string;
+    className?: string;
+    disabled?: boolean;
+    mode?: 'fill' | 'outline';
 }
 
-export default (props : Props) =>{
-    const classes = [styles.container , props.className]
-   if(props.mode == 'outline') classes.push(styles.outline)
-   else classes.push(styles.fill)
+export default (props: Props) => {
+    const classes = [styles.container, props.className]
+    if (props.mode == 'outline') classes.push(styles.outline)
+    else classes.push(styles.fill)
 
-    return(
+    return (
         <button disabled={props.disabled}
-         className={classes.join(' ').trim()}
-         >
+            className={classes.join(' ').trim()}
+        >
             {props.title}
         </button>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ import styles from './page.module.css'
 import Button from './componets/Button/Button'
 
 export default function Home() {
-  return (
-   <main className={styles.main}>
+   return (
+      <main className={styles.main}>
 
-      <div className={styles.center}>
-         <Button mode='outline' title='რეგისტრაცია'/>
-      </div>
-      </main> 
-  )
+         <div className={styles.center}>
+            <Button mode='outline' title='რეგისტრაცია' />
+         </div>
+      </main>
+   )
 }
